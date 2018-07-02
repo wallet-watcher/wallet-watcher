289391cc288054f0d0f4c3ae591221b681c916ac
Yasin Shuman <email@yasin.io>
2018-07-02 10:41:11 -0700

    added axios

--- server/util/twilio.js
+++ server/util/twilio.js
@@ -1,38 +1,37 @@
-//auth twilio client
-var authToken = "d495fdae5d1bf3d80b53e680480fc8b3";
-const accountSid = "AC6f11471cb18f487bb1f4a6c6df9e1ff3";
-const client = require("twilio")(accountSid, authToken);
+const twilio = require("../config/twilioConfig");
+const client = require("twilio")(twilio.accountSid, twilio.authToken);
+const fromSms = twilio.fromSms;
 
-// TODO: check address valid
-/* TODO: 
+const toSms = "+16286008772";
+const transAmount = " 1 eth";
+const address = "0xE2213989f81EeEFc8C3577554083c8B6b8a1032c";
+const walletBalance = "10 eth";
 
-1. get wallet address from db
-2. if wallet balance has changed and falls within threshold for alert
-3. send text to number stored in db
+function sendIncreaseSMS(address, walletBalance, transAmount, toSms, fromSms) {
+  return client.messages
+    .create({
+      body: `\nALERT: ${address} \n Has Increased by ${transAmount}. Balance is now: ${walletBalance}`,
+      to: toSms, // Text this number
+      from: fromSms // From a valid Twilio number
+    })
+    .then(message => console.log(message.sid));
+}
 
-*/
+function sendDecreaseSMS(address, walletBalance, transAmount, toSms, fromSms) {
+  return client.messages
+    .create({
+      body: `\nALERT: ${address} \n Has Increased by ${transAmount}. Balance is now: ${walletBalance}`,
+      to: toSms, // Text this number
+      from: fromSms // From a valid Twilio number
+    })
+    .then(message => console.log(message.sid));
+}
 
-// function sendSMS(text, toSms, fromSms) {
-//   return client.messages
-//     .create({
-//       body: text,
-//       to: toSms, // Text this number
-//       from: fromSms // From a valid Twilio number
-//     })
-//     .then(message => console.log(message.sid));
-// }
+sendIncreaseSMS(text, toSms, fromSms);
 
-// const createWallet = (req, res) => {
-//   console.log("creating a wallet");
-//   //TODO: check if merchant has enough btc to sell to customer
+sendDecreaseSMS(text, toSms, fromSms);
 
-//   const { toSms } = req.body;
-//   const newWallet = wallet();
-//   const { address, mnemonic, privateKey } = newWallet;
-//   sendSMS(
-//     `\nWallet Address: ${address} \n \nPrivate Key ${privateKey}\n\n Mnemonic: ${mnemonic}`,
-//     toSms,
-//     fromSms
-//   );
-//   res.status(200).json(newWallet);
-// };
+module.exports = {
+  sendIncreaseSMS,
+  sendDecreaseSMS
+};
