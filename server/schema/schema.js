f2e67ae88cae47c8b268965edfb1174aca5612f4
Justin Kaseman <justinkaseman@live.com>
2018-07-02 16:49:23 -0700

    Some changes

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,12 +1,23 @@
 const typeDefs = `
     type User { 
+        _id: String!,
         address: String!,
         number: Int!,
         balance: Int!
     }
 
     type Query {
-        users: [User]
+        users: [User],
+        user(_id: String): User
+    }
+
+    type Mutation {
+        createUser(address: String, number: String, balance: Int): User
+    }
+
+    schema {
+        query: Query
+        mutation: Mutation
     }
 `;
 
