c1e481f448c67a9f1c3cff416d790be4f37e0d49
Yasin Shuman <email@yasin.io>
2018-07-02 15:07:58 -0700

    handing off

--- server/util/eth.js
+++ server/util/eth.js
@@ -22,24 +22,41 @@ function getBal(addressArray) {
     });
 }
 
-function validateAddress(oneAddress) {
+async function validateAddress(oneAddress) {
   let URL = etherScan.etherScanSingleURL + oneAddress + etherScan.etherScanURL2;
-  let bool = true;
-  axios
-    .get(URL)
-    .then(response => {
-      console.log(response.data.status);
-      if ((response.data.status = 0)) {
-        return false;
-      } else if ((response.data.status = 1)) {
-        return true;
+  let isItGood = true;
+  try {
+    let response = await axios.get(URL);
+    // let resCode = response.data.status;
+    // console.log(resCode);
+    if (response.status == 200) {
+      if (response.data.status == 0) {
+        console.log("bad address");
+        isItGood = false;
+      } else if (response.data.status == 1) {
+        console.log("good address");
+        isItGood = true;
       }
-    })
-    .catch(err => {
-      console.log(err);
-    });
+      return isItGood;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+  //   if (resCode === 0) {
+  //     console.log("bad address");
+  //     return false;
+  //   } else if (resCode === 1) {
+  //     console.log("good address");
+  //     return true;
+  //   }
+  // })
+  // .catch(err => {
+  //   console.log(err);
+  // });
 }
-if (validateAddress("0xe2213989f81eeefc8c3577554083c8b6b8a1032c")) {
-  console.log("this is true");
-} else console.log("this is false");
+
+let ggg = validateAddress("0xe2213989f81eeefc8c3577554083c8b6b8a1032c");
+
+console.log(ggg == true);
 // getBal(addressArray);
