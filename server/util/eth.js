84f332b90fa4bc8c32811afe92bde6e795f3017b
Troy Williams <troyDonwilliams@gmail.com>
2018-07-02 23:44:28 -0500

    change server to use just express and mongoose. no graphql yet

--- server/util/eth.js
+++ server/util/eth.js
@@ -15,13 +15,43 @@ function getBal(addressArray) {
     });
 }
 
+// function getSingleBal(singleAddress) {
+//   let address = String(singleAddress);
+//   let URL = etherScan.etherScanSingleURL + address + etherScan.etherScanURL2;
+//   return axios
+//     .get(URL)
+//     .then(response => {
+//       return response.data.result;
+//     })
+//     .catch(err => {
+//       console.log(err);
+//     });
+// }
+
+async function getSingleBal(singleAddress) {
+  let address = String(singleAddress);
+  let URL = etherScan.etherScanSingleURL + address + etherScan.etherScanURL2;
+  try {
+    const balance = await axios
+      .get(URL)
+      .then(response => {
+        return response.data.result;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+    return balance;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 function validateAddress(oneAddress) {
   let URL = etherScan.etherScanSingleURL + oneAddress + etherScan.etherScanURL2;
 
   return axios
     .get(URL)
     .then(function(response) {
-      console.log(response.data.status);
       if (response.data.status == 0) {
         return false;
       } else if (response.data.status == 1) {
@@ -33,11 +63,12 @@ function validateAddress(oneAddress) {
     });
 }
 
-// validateAddress('0xe2213989f81eeefc8c3577554083c8b6b8a1032c').then(response => {
+// validateAddress("0xE2213989f81EeEFc8C3577554083c8B6b8a1032c").then(response => {
 //   console.log(response);
 // });
-
+// getSingleBal("0xe2213989f81eeefc8c3577554083c8b6b8a1032c");
 module.exports = {
   getBal,
+  getSingleBal,
   validateAddress
 };
--- server/util/twilio.js
+++ server/util/twilio.js
@@ -1,9 +1,9 @@
 const twilio = require('../config/twilioConfig');
 const client = require('twilio')(twilio.accountSid, twilio.authToken);
 
-const fromSms = twilio.fromSms;
+const { fromSms } = twilio;
 
-function sendIncreaseSMS(address, walletBalance, transAmount, toSms, fromSms) {
+function sendIncreaseSMS(address, walletBalance, transAmount, toSms) {
   return client.messages
     .create({
       body: `\nALERT: ${address} \n Has Increased by ${transAmount}. Balance is now: ${walletBalance}`,
@@ -12,8 +12,17 @@ function sendIncreaseSMS(address, walletBalance, transAmount, toSms, fromSms) {
     })
     .then(message => console.log(message.sid));
 }
+function sendWelcomeSMS(address, toSms) {
+  return client.messages
+    .create({
+      body: `\nYou are now montioring address: ${address} \n `,
+      to: toSms, // Text this number
+      from: fromSms // From a valid Twilio number
+    })
+    .then(message => console.log(message.sid));
+}
 
-function sendDecreaseSMS(address, walletBalance, transAmount, toSms, fromSms) {
+function sendDecreaseSMS(address, walletBalance, transAmount, toSms) {
   return client.messages
     .create({
       body: `\nALERT: ${address} \n Has Increased by ${transAmount}. Balance is now: ${walletBalance}`,
@@ -25,5 +34,6 @@ function sendDecreaseSMS(address, walletBalance, transAmount, toSms, fromSms) {
 
 module.exports = {
   sendIncreaseSMS,
-  sendDecreaseSMS
+  sendDecreaseSMS,
+  sendWelcomeSMS
 };
